9ae02468d14e31ad0564b04622420987c30d187b
dupasj <dupasj97@gmail.com>
2020-08-25 13:59:10 +0200

    update

--- components/WeeklyTeam.vue
+++ components/WeeklyTeam.vue
@@ -5,6 +5,12 @@
       <h2>LDV Esport</h2>
       <p class="description">“Première équipe universitaire d’e-sport en France, cette association esport s’est faite remarquer à maintes reprises depuis déjà 4 ans. Une équipe qui excelle en compétition et à le coeur sur la main, comme Dernièrement avec la 14aine e-sport !”</p>
 
+      <ul class="statistiques">
+        <li class="hourglass-icon">5 ans</li>
+        <li class="pin-icon">Courbevoie</li>
+        <li class="user-icon">+ de 30 personnes</li>
+      </ul>
+
       <div class="button-container">
         <a class="button">En savoir plus</a>
       </div>
@@ -14,6 +20,42 @@
 </template>
 
 <style>
+
+
+.statistiques{
+  padding:24px 0 33px 0;
+  width:100%;
+  height:auto;
+  display:block;
+  margin: 0;
+  list-style: none;
+  text-align: left;
+}
+.statistiques li{
+  display:inline-block;
+  padding: 2px 24px 2px 30px;
+  margin: 0;
+  line-height:20px;
+  font-size:17px;
+  color: #ffffff;
+  font-family: "Helvetica Neue",sans-serif;
+  text-align: left;
+
+  background-position: left center;
+  background-repeat: no-repeat;
+  background-size: 20px;
+}
+.statistiques li.hourglass-icon{
+  background-image: url("../assets/icon/hourglass.png");
+}
+.statistiques li.user-icon{
+  background-image: url("../assets/icon/user.png");
+}
+.statistiques li.pin-icon{
+  background-image: url("../assets/icon/pin.png");
+}
+
+
 .centred-title-container{
   width:100%;
   height:auto;
@@ -99,7 +141,7 @@ h3:after{
   line-height: 17px;
   font-size:16px;
   text-align: left;
-  margin:28px 0;
+  margin:0 0 28px 0;
 }
 
 .right:before{
@@ -118,7 +160,7 @@ h3:after{
   width:100%;
   height:auto;
   text-align: center;
-  margin:50px 0;
+  margin:0 0 50px 0;
 }
 
 .button{
