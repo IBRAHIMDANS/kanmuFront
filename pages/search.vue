9ff53f0ec472c3676b4c82c8b4ff14498d302600
dupasj <jeremiedupas@yahoo.fr>
2020-10-09 11:58:12 +0200

    Initial commit

---
+++ pages/search.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="left">
+      <div class="title-container">
+        <h1>Kanmu</h1>
+      </div>
+      <div class="search-container">
+        <input type="search" placeholder="Rechercher votre équipe">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({})
+</script>
+
+<style>
+body{
+  width:100%;
+  height:auto;
+  min-height:100%;
+  padding: 0;
+  margin: 0;
+  position: relative;
+}
+html{
+  width:100%;
+  height:100vh;
+  overflow-x: hidden;
+  overflow-y: auto;
+}
+</style>
